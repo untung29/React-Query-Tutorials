dd17ccc89757f73b4eb633a634ff188854e1af4c
Untung <u.untung0029@gmail.com>
2022-02-10 23:28:58 +1100

    Learn RQ

--- react-query-demo/src/App.js
+++ react-query-demo/src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { QueryClientProvider, QueryClient } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import './App.css'
-import { HomePage } from './components/Home.page'
-import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
-import { RQSuperHeroPage } from './components/RQSuperHero.page'
-import { SuperHeroesPage } from './components/SuperHeroes.page'
-import { ParallelQueriesPage } from './components/ParallelQueries.page'
-import { DynamicParallelPage } from './components/DynamicParallel.page'
-import { DependentQueriesPage } from './components/DependentQueries.page'
-import { PaginatedQueriesPage } from './components/PaginatedQueries.page'
-import { InfiniteQueriesPage } from './components/InfiniteQueries.page'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-const queryClient = new QueryClient()
+import './App.css';
+import { HomePage } from './components/Home.page';
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
+import { RQSuperHeroPage } from './components/RQSuperHero.page';
+import { SuperHeroesPage } from './components/SuperHeroes.page';
+import { ParallelQueriesPage } from './components/ParallelQueries.page';
+import { DynamicParallelPage } from './components/DynamicParallel.page';
+import { DependentQueriesPage } from './components/DependentQueries.page';
+import { PaginatedQueriesPage } from './components/PaginatedQueries.page';
+import { InfiniteQueriesPage } from './components/InfiniteQueries.page';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -22,50 +23,50 @@ function App() {
           <nav>
             <ul>
               <li>
-                <Link to='/'>Home</Link>
+                <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+                <Link to="/super-heroes">Traditional Super Heroes</Link>
               </li>
               <li>
-                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+                <Link to="/rq-super-heroes">RQ Super Heroes</Link>
               </li>
             </ul>
           </nav>
           <Switch>
-            <Route path='/super-heroes'>
+            <Route path="/super-heroes">
               <SuperHeroesPage />
             </Route>
-            <Route path='/rq-super-heroes/:heroId'>
+            <Route path="/rq-super-heroes/:heroId">
               <RQSuperHeroPage />
             </Route>
-            <Route path='/rq-super-heroes'>
+            <Route path="/rq-super-heroes">
               <RQSuperHeroesPage />
             </Route>
-            <Route path='/rq-parallel'>
+            <Route path="/rq-parallel">
               <ParallelQueriesPage />
             </Route>
-            <Route path='/rq-dynamic-parallel'>
+            <Route path="/rq-dynamic-parallel">
               <DynamicParallelPage heroIds={[1, 3]} />
             </Route>
-            <Route path='/rq-dependent'>
-              <DependentQueriesPage email='vishwas@example.com' />
+            <Route path="/rq-dependent">
+              <DependentQueriesPage email="vishwas@example.com" />
             </Route>
-            <Route path='/rq-paginated'>
+            <Route path="/rq-paginated">
               <PaginatedQueriesPage />
             </Route>
-            <Route path='/rq-infinite'>
+            <Route path="/rq-infinite">
               <InfiniteQueriesPage />
             </Route>
-            <Route path='/'>
+            <Route path="/">
               <HomePage />
             </Route>
           </Switch>
         </div>
       </Router>
-      <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- react-query-demo/src/components/RQSuperHeroes.page.js
+++ react-query-demo/src/components/RQSuperHeroes.page.js
@@ -1,71 +1,40 @@
-import { useState } from 'react'
-import {
-  useAddSuperHeroData,
-  useSuperHeroesData
-} from '../hooks/useSuperHeroesData'
-import { Link } from 'react-router-dom'
+/**
+ * Homework
+ * Polling + Callback
+ * refetchInterval option every 3 seconds
+ * add a new superhero
+ * If the new superhero is 4, then stop pulling, and if error stop pulling too
+ */
+
+import { useSuperHeroesData } from '../hooks/useSuperHeroesData.js';
 
 export const RQSuperHeroesPage = () => {
-  const [name, setName] = useState('')
-  const [alterEgo, setAlterEgo] = useState('')
-
-  const onSuccess = data => {
-    console.log({ data })
-  }
+  const onSuccess = data => {};
 
   const onError = error => {
-    console.log({ error })
-  }
+    console.log(error);
+  };
 
-  const { isLoading, data, isError, error, refetch } = useSuperHeroesData(
-    onSuccess,
-    onError
-  )
+  const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError);
 
-  const { mutate: addHero } = useAddSuperHeroData()
-
-  const handleAddHeroClick = () => {
-    const hero = { name, alterEgo }
-    addHero(hero)
-  }
-
-  if (isLoading) {
-    return <h2>Loading...</h2>
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>;
   }
 
   if (isError) {
-    return <h2>{error.message}</h2>
+    return <h2>{error.message}</h2>;
   }
 
   return (
     <>
-      <h2>React Query Super Heroes Page</h2>
-      <div>
-        <input
-          type='text'
-          value={name}
-          onChange={e => setName(e.target.value)}
-        />
-        <input
-          type='text'
-          value={alterEgo}
-          onChange={e => setAlterEgo(e.target.value)}
-        />
-        <button onClick={handleAddHeroClick}>Add Hero</button>
-      </div>
-      <button onClick={refetch}>Fetch heroes</button>
-      {data?.data.map(hero => {
-        return (
-          <div key={hero.id}>
-            <Link to={`/rq-super-heroes/${hero.id}`}>
-              {hero.id} {hero.name}
-            </Link>
-          </div>
-        )
-      })}
-      {/* {data.map(heroName => {
-        return <div key={heroName}>{heroName}</div>
+      <h2>RQ Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes!</button>
+      {/* {data?.data.map(hero => {
+        return <div key={hero.name}>{hero.name}</div>;
       })} */}
+      {data.map(heroName => {
+        return <div key={heroName}>{heroName}</div>;
+      })}
     </>
-  )
-}
+  );
+};
